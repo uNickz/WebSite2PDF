300070647a8c6ff8627a62ad9aa08639a7f26d09
uNickz <52837683+uNickz@users.noreply.github.com>
2023-01-07 14:34:13 +0100

    Update example.py

--- Example/example.py
+++ Example/example.py
@@ -1,6 +1,11 @@
 import WebSite2PDF
 
 if __name__ == "__main__":
-    c = WebSite2PDF.Client()
+    c = WebSite2PDF.Client(
+        pdfOptions = {
+            "printBackground": True,
+            "preferCSSPageSize": True,
+        },
+    )
     pdf_path = c.pdf(url = "https://pypi.org", filename = "{title}.pdf")
     print(f"PDF Created! Path: {pdf_path}")
