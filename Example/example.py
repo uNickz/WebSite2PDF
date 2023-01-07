324ba87f68222a475594778a2b80db9a281e58ec
uNickz <52837683+uNickz@users.noreply.github.com>
2023-01-07 12:04:54 +0100

    Create example.py

---
+++ Example/example.py
@@ -0,0 +1,6 @@
+import WebSite2PDF
+
+if __name__ == "__main__":
+    c = WebSite2PDF.Client()
+    pdf_path = c.pdf(url = "https://pypi.org", filename = "file_name.pdf")
+    print(f"PDF Created! Path: {pdf_path}")
