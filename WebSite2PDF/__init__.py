79b8eecaf76c0a5464c7e08aec4e86d83fc2838f
uNickz <52837683+uNickz@users.noreply.github.com>
2023-12-23 11:28:01 +0100

    Update __init__.py

--- WebSite2PDF/__init__.py
+++ WebSite2PDF/__init__.py
@@ -1,5 +1,5 @@
 from .client import Client
 
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2023 uNickz <https://github.com/uNickz>"
